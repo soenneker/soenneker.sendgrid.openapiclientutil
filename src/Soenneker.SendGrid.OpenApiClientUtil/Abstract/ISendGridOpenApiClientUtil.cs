89a77c5b4a3a9623385d771e7d412ccc5743ac91
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-06-04 19:38:43 -0500

    xml commenting

--- src/Soenneker.SendGrid.OpenApiClientUtil/Abstract/ISendGridOpenApiClientUtil.cs
+++ src/Soenneker.SendGrid.OpenApiClientUtil/Abstract/ISendGridOpenApiClientUtil.cs
@@ -10,5 +10,10 @@ namespace Soenneker.SendGrid.OpenApiClientUtil.Abstract;
 /// </summary>
 public interface ISendGridOpenApiClientUtil: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Gets the value.
+    /// </summary>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>A task containing the result of the operation.</returns>
     ValueTask<SendGridOpenApiClient> Get(CancellationToken cancellationToken = default);
 }
